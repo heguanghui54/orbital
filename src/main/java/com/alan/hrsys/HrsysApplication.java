ad1321ec79ebc15aacedb524480d930c6091613f
heguanghui <hgh54913@gmail.com>
2024-05-10 13:50:25 +0800

    Initial commit of all project files

---
+++ src/main/java/com/alan/hrsys/HrsysApplication.java
@@ -0,0 +1,11 @@
+package com.alan.hrsys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrsysApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(HrsysApplication.class, args);
+    }
+}
